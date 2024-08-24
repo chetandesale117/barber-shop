d2e984a4cc433434a6c1eff3b459b100223e8a90
cheta desale <chetandesale117@gmail.com>
2024-08-24 13:35:09 +0530

    initial commit

---
+++ src/components/Welcome.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+const Welcome = () => {
+  return (<>
+  
+  <div className='welcome'>
+        <h1>Welcome To My Shop</h1>
+    </div>
+  </>
+    )
+}
+
+export default Welcome;
